511c4e20be121f3a42fe14d64eb138684e42a8b7
Justin179 <justin.uob@gmail.com>
2017-03-03 15:12:46 +0800

    ListAdapter

--- app/src/main/java/com/example/justin179/notebook/MainActivityListFragment.java
+++ app/src/main/java/com/example/justin179/notebook/MainActivityListFragment.java
@@ -5,6 +5,7 @@ import android.app.ListFragment;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 
@@ -17,6 +18,20 @@ public class MainActivityListFragment extends ListFragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState){
         super.onActivityCreated(savedInstanceState);
+
+        // String Array to represent rows
+        String[] values = new String[] {"A","B","C","D","E"};
+
+        // put context, format, content(String Array) into adapter
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,values);
+
+        // put adapter into the list
+        setListAdapter(arrayAdapter);
+
+
+
+
+
     }
 
     @Override
